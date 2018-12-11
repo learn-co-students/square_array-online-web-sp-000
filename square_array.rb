620de60969659b78166df592617376bf8ea52dd6
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-12-11 06:35:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared_array = []
   array.each do |element|
-    squared_array << (element * element)
+    squared_array << element * element
   end
   squared_array
 end
