c5800b6522d538214c4cff91f2224d8fb48089b6
Danielle Bergbower <dkw5011@gmail.com>
2019-04-21 18:08:18 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |number|
+  squared_number = (number * number)
+  
+  squared_array << (squared_number)
+  
+end
+squared_array
 end
