8f92a70d3509853ff7b9b26b991f1ea7c4306a1f
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-21 00:08:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
+
 def square_array(array)
-  # your code here
+squared_array = []
+  array.each do |element|
+    squared_element = element ** 2
+    squared_array.push(squared_element)
+  end
+  return squared_array
 end
