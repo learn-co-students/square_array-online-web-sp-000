7e8ec1ad5b409e43ae096c5eee098d77860ca6b7
Christopher Dent <christopherdent01@gmail.com>
2019-07-21 00:31:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
-def square_array(array)
-  # your code here
-end
+
+
+def square_array(numbers)
+  
+  new_array = []
+  
+    numbers.each do |number|
+      
+      new_array << number ** 2
+    
+    end 
+          
+  new_array
+    
+end 
+
+
+
+###the block of code from do, to end, returns the original array.  However the method returns 1, 2, 3
+
