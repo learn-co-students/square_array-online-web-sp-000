7a7bcc6203d7377ee5dd3c97bcc3d70458bdfad6
Panela Leung <leungpanela@gmail.com>
2019-08-23 23:03:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+require "pry"
+
 def square_array(array)
   # your code here
+  new_array= Array.new
+  array.each do |num|
+    operation = num ** 2
+    new_array << operation
+  end
+return new_array
 end
