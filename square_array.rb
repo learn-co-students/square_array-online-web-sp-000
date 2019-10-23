18ee306d22fdc470bd55a62bda0df13aa15a2e7a
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-23 16:24:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+require "pry"
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |num|
+
+#multiply values by themselves and add to new array
+    new_array.push(num ** 2)
+  end
+  return new_array
 end
+
+  #new_array << square_array[array] ** 2
+#array.each do {|num| num **2}
