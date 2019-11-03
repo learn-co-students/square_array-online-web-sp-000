584b0fde4d63969a07c0d16076cc2b812a607f59
github username <github email address>
2019-11-03 01:43:42 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-  array.each do |square_array_element|
-    square_array_element * square_array_element
-    /
-  end
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
+  
 end
