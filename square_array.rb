5e2ca5d3a80754e2e9db41ae2625979fb6553952
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-02 15:38:53 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,6 @@ require 'pry'
 def square_array(array)
   # your code here
   new_array = []
-  array.each do |number|
-    new_array << number ** 2 
-  end 
+  array.each {|number| new_array << number ** 2}
   new_array
 end
