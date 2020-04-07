91c90de8b43c5790078edae95d3ad5f87d84fe52
Evan Burton <evan@heyevan.com>
2020-04-07 07:23:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,15 @@
 def square_array(array)
-new_array = []
-array.each do |index|
-  new_array.push(index ** 2)
+  new_array = []
+  array.each do |index|
+    new_array.push(index ** 2)
   end
   return new_array
-end
+end 
+
+
+
+
+#def square_array(array)
+#new_array = array.collect {|index| index ** 2}
+#return new_array
+#end
