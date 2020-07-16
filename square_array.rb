69764ac1915266810f9deadab5f278ea4bfafa74
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-16 15:30:15 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  new_array = []
-  array.each do |number|
-    square = number ** 2
-    new_array.push(square)
+  new_array = [] # creates a new array
+  array.each do |number| # using the #each method, 
+    square = number ** 2 # the array is iterated over and each number squared
+    new_array.push(square) # then the result is passed to the new array
   end
-  new_array
+  new_array # this is necessary to cause #square_array to return the new array
 end
