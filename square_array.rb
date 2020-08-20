d381969b9c8fe8a1f127240859c41e85996d1d0d
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 18:50:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number|
+    new_array << number ** 2
+  end
+  new_array
 end
+
+def square_array_collect(array)
+  array.collect {|number| number ** 2}
+end
+
+def square_array_map(array)
+  array.map {|number| number ** 2}
+end
+
+a = [1,2,3,4]
+puts square_array_collect(a).inspect
