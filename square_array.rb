36492570acc89fa08d2e9d2799afb3d4044588c2
Alie Stone <helloaliestone@gmail.com>
2020-09-09 17:10:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+    new_array  = []
+    array.each do |n|
+    n=n**2
+    new_array << n
+  end
+    return new_array
 end
+
+
+#return value of calling each on an array
