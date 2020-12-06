c1762a9ceebe1caedb2f015f9e881eba227f204e
remote-torrent-6747 <abbyc@sky.com>
2020-12-06 20:59:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  squared_numbers = []
+  array.each do |number|
+    result = number ** 2
+    squared_numbers.push(result)
+  end
+  return squared_numbers
 end
