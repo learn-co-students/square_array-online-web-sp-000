7e61a04e57283d51cca4314111d9d220cbaf1312
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-04 02:42:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+Enumerator.new do |y|
+numbers = [1,2,3]
+array.each {|a| y << a**2}
 end
+.take(array.length)
+end
+#[].tap do |a|
+#def square_array(new_numbers)
+#new_number = [9,10,15,25]
+ #square_array(new_numbers)
