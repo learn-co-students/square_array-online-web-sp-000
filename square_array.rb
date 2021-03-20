352751592be23c18e03babd3b31b444784274f64
Craig <suesscraig112715@gmail.com>
2021-03-20 19:15:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code helper_method
-  new_array = [];
-  array.each do |numbers|
-    new_array<<numbers**2;
+  new_array = [];                           #make a new array to store new values 
+  array.each do |numbers|                 
+    new_array<<numbers**2;                  #itterate through the passed in array, push the                                               squared numbers to the new array. 
   end
-  return new_array
+   new_array                          
 end
+
