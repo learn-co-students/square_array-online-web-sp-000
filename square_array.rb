552552ad99669bcaa6458081360488fe79d27bd2
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-02 03:31:32 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+   new_array = []
+   array.each do |element| 
+     new_array << element * element
+   end
+   return new_array
 end
+
+
+
+
+
